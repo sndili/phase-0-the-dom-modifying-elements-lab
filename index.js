8248ea5e45694a2721dd87927b3cc0ec02073c38
Sylvia <ndilisylvia@gmail.com>
2022-08-04 13:12:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName('main')[0];
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = "Ndili is the champion"
